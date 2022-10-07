b2a6588eff41d893a2c16e3b3f6c1da63ddd07ad
Sotatek-TrungPhan <trung.phan@sotatek.com>
2022-10-07 15:56:29 +0700

    feat/initialize database

--- orders/src/database/order-database-init.sh
+++ orders/src/database/order-database-init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -e
 psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" --dbname "$POSTGRES_DB" <<-EOSQL
-    CREATE DATABASE payment-management;
+    CREATE DATABASE order-management;
 EOSQL
